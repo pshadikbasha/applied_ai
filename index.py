3ec29668ab777012a805241a5100ae596bf0be07
pshadikbasha <pshadikbasha97@gmail.com>
2022-01-16 19:43:58 +0530

    numpy operations

---
+++ index.py
@@ -0,0 +1,11 @@
+""" import keyword
+print(keyword.kwlist)
+print(len(keyword.kwlist)) """
+""" import numpy as np
+a=np.array([1,2,3,4,5])
+print(a)
+a=np.arange(10)
+print(a) """
+l=range(1000)
+print([i**2 for i in l])
+    
